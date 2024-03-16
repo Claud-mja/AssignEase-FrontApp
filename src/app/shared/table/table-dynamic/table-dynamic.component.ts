6fd254fbcec590afc8b69c78fe2590679fc2c9be
Claud Mja <claudmja2.0@gmail.com>
2024-03-16 21:35:34 +0300

    [dev] create table-dynamic component

---
+++ src/app/shared/table/table-dynamic/table-dynamic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-dynamic',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-dynamic.component.html',
+  styleUrl: './table-dynamic.component.css'
+})
+export class TableDynamicComponent {
+
+}

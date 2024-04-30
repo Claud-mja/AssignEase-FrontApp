cf59bb139579beb0d786a6379a392c47b861ce09
FranciscoNoam <antoenjara1998@gmail.com>
2024-04-30 11:41:00 +0300

    list sutdent terminer mais à améliorer

--- src/app/components/user-space/student/student.component.ts
+++ src/app/components/user-space/student/student.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import {MatTableModule} from '@angular/material/table';
 
 import { Auteur } from '../../../shared/models/auteur.model';
 
@@ -10,10 +11,13 @@ import { AuteurService } from '../../../shared/services/auteur/auteur.service';
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
-  styleUrls: ['./student.component.css']
+  styleUrls: ['./student.component.css'],
+  standalone: true,
+  imports: [MatTableModule],
 })
 export class StudentComponent {
   auteurs: Auteur[] = []; // Initialisez auteurs avec un tableau vide
+  displayedColumns: string[] = ['demo-position', 'demo-name', 'demo-weight', 'demo-weight'];
 
 
   constructor(private auteurService: AuteurService) { }

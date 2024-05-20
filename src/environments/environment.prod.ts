427497d0e6622db8448ee9332e2da8f2dc9294cd
Claud Mja <claudmja2.0@gmail.com>
2024-05-20 04:01:10 +0300

    debug : change environement prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api.semwee.app:3000/api',
-  baseUrlImg: 'https://api.semwee.app:3000',
+  baseUrl: 'https://assignease-backend-b8rt.onrender.com/api',
+  baseUrlImg: 'https://assignease-backend-b8rt.onrender.com/public/img',
 };
